b0aa2576ed19ca169491a0ca4dd17228fbb0576c
damolabolaji <bolaji.oyindamola@gmail.com>
2022-10-11 16:08:37 +0100

    added prompt for the user

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 let month = Number(prompt("please enter a number from 1 - 12"))
 
-if (Number.isNaN(month)){
-    Number(prompt("you need to enter a number from 1 - 12"))
-} else if()
+while (Number.isNaN(month)){
+    Number(prompt("you need to enter a number from 1 - 12"));
+} 
+
