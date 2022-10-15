084a0c9b55d590e9a6d09d6062419ec9e1e21f3d
damolabolaji <bolaji.oyindamola@gmail.com>
2022-10-15 01:41:15 +0100

    MODELING BANK ACCOUNT exercixe added

--- objects.js
+++ objects.js
@@ -57,3 +57,31 @@ const circle = {
 
 console.log(`Its circumference is ${circle.circumference()}`);
 console.log(`Its area is ${circle.area()}`);
+
+
+//MODELING BANK ACCOUNT
+// Write a program that creates an account object with the following characteristics:
+
+
+const accountHolder = {
+    name: 'Alex',
+    balance: 0,
+    describe(){
+        return `Owner: ${accountHolder.name}, balance: ${accountHolder.balance}`
+    },
+    credit(addCredit){
+        this.balance += addCredit
+        return this.balance
+    }
+}
+
+console.log( accountHolder.describe())
+console.log( accountHolder.credit(250))
+console.log( accountHolder.credit(-80))
+console.log( accountHolder.describe())
+
+// A name property set to "Alex".
+// A balance property set to 0.
+// A credit method adding the (positive or negative) value passed as an argument to the account balance.
+// A describe method returning the account description.
+// Use this object to show its description, crediting 250, debiting 80, then show its description again.
